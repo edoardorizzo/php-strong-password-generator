c3642d5d803d7904c20695e5fee7c0da0aa386be
edoardorizzo <edo96.rizzo@gmail.com>
2023-05-05 17:42:24 +0200

    refactoring psw genertor

--- functions.php
+++ functions.php
@@ -1,18 +1,19 @@
 <?php
+        // Controlla se è stata inviata la richiesta
+        if(isset($_GET['number'])){
+            // Funzione per generare la password casuale
+            function randomPassword() {
+                $alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
+                $pass = array(); //remember to declare $pass as an array
+                $alphaLength = strlen($alphabet) - 1; //put the length -1 in cache
+                for ($i = 0; $i < $_GET['number']; $i++) {
+                    $n = rand(0, $alphaLength);
+                    $pass[] = $alphabet[$n];
+                }
+                return implode($pass);
+            }
 
-//echo $_GET['number'];
-
-function randomPassword() {
-    $alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
-    $pass = array(); //remember to declare $pass as an array
-    $alphaLength = strlen($alphabet) - 1; //put the length -1 in cache
-    for ($i = 0; $i < $_GET['number']; $i++) {
-        $n = rand(0, $alphaLength);
-        $pass[] = $alphabet[$n];
-    }
-    return implode($pass); //turn the array into a string
-}
-
-echo randomPassword();
-
-?>
+            $password = randomPassword();
+            echo "<p>Password generata: $password</p>";
+        }
+    ?>
